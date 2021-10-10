f91de545a030310d3586bd2e2bb1f5a845c28197
Samuel Neaves <sn0550@IT017509.lan>
2021-10-10 21:43:12 +0100

    Add scasp integration and arity of body terms

--- simply_logical/hyp_gen.pl
+++ simply_logical/hyp_gen.pl
@@ -1,16 +1,28 @@
-modeb(penguin).
-modeb(bird).
-modeb(has_feathers).
-modeb(has_beak).
+% Your program goes here
+% s(CASP) Programming
+:- use_module(library(scasp)).
+
+% Your program goes here
+
+
+modeb(penguin/1).
+modeb(bird/1).
+modeb(has_feathers/1).
+modeb(has_beak/1).
+modeb(has_car/2).
+
+
 
 %%%mayb include the numbering of vars in this generation.
 %%% Insure vars form dag of unification.
-hypothesis_body([X]):-modeb(X).
-hypothesis_body([not(X)]):-modeb(X).
-hypothesis_body([X|Xs]):- maplist(dif(X),Xs),modeb(X), hypothesis_body(Xs). %mayb retract and replace this clause with specialisations.
+hypothesis_body([X/Arity]):-modeb(X/Arity).
+hypothesis_body([not(X/Arity)]):-modeb(X/Arity).
+hypothesis_body([X/Arity|Xs]):- maplist(dif(X/Arity),Xs),modeb(X/Arity), hypothesis_body(Xs). %mayb retract and replace this clause with specialisations.
+%%%hypothesis_body([X/Arity|Xs]):- modeb(X/Arity),not member(X/Arity,Xs),hypothesis_body(Xs). %mayb retract and replace this clause with specialisations.
 
-thing(not(X),Y,not(Term)):- Term =..[X,Y].
-thing(X,Y,Term):- atom(X),Term =..[X,Y].
+
+thing(not(X/Arity),_,not(Term)):- length(List,Arity), append([X],List,Total),Term =..Total.
+thing(X/Arity,_,Term):- atom(X),length(List,Arity), append([X],List,Total),Term =..Total.
 
 hypothesis_body_vars(X,Terms):-
     length(X,Len),

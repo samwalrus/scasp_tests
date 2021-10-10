e345c763dbb8a3259cc8458c146bc9a93d191f50
Samuel Neaves <sn0550@IT017509.lan>
2021-10-10 21:49:03 +0100

    Think about input/output and constants

--- simply_logical/hyp_gen.pl
+++ simply_logical/hyp_gen.pl
@@ -4,6 +4,10 @@
 
 % Your program goes here
 
+%%%need to add 
+%%% + for input vars
+%%% - for output vars
+%%% # for constant
 
 modeb(penguin/1).
 modeb(bird/1).
@@ -20,6 +24,14 @@ hypothesis_body([not(X/Arity)]):-modeb(X/Arity).
 hypothesis_body([X/Arity|Xs]):- maplist(dif(X/Arity),Xs),modeb(X/Arity), hypothesis_body(Xs). %mayb retract and replace this clause with specialisations.
 %%%hypothesis_body([X/Arity|Xs]):- modeb(X/Arity),not member(X/Arity,Xs),hypothesis_body(Xs). %mayb retract and replace this clause with specialisations.
 
+%%% Gen a hyp
+%%% Add another clause?
+
+
+%%% spec a hyp
+%%% unify or constrain vars.
+
+
 
 thing(not(X/Arity),_,not(Term)):- length(List,Arity), append([X],List,Total),Term =..Total.
 thing(X/Arity,_,Term):- atom(X),length(List,Arity), append([X],List,Total),Term =..Total.
@@ -30,3 +42,8 @@ hypothesis_body_vars(X,Terms):-
     maplist(thing,X,Yvars,Terms).
 
 %?- hypothesis_body(X),hypothesis_body_vars(X,Terms).
+
+
+/** <examples> Your example queries go here, e.g.
+?- ? p(X).
+*/
